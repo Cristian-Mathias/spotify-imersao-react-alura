32a7902b3038b28c336478415a9ed365513abb42
Cristian-Mathias <cristian11mathias@hotmail.com>
2025-01-31 14:07:09 -0300

    Criando o componente Main

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Sidebar></Sidebar>
       <Header></Header>
+      <Main></Main>
       <Footer></Footer>
     </>
   )
